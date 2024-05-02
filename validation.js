7903a7990becdd0cb7a207e46e1b140ecb3c8b42
Vinicius Alves <viniciusalvesms757@gmail.com>
2024-05-02 20:47:11 -0300

    Front-end commit

---
+++ validation.js
@@ -0,0 +1,64 @@
+// Elements
+const inputElement = document.querySelector("input");
+const formElement = document.querySelector("form");
+const modalElement = document.querySelector(".sucess-modal")
+const emailTextElement = document.querySelector("#emailTextElement")
+
+// Variables
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+let isEmailValid = false;
+let email = "";
+
+// Validation
+const emailValidation = () => {
+
+    if(inputElement.value.toLowerCase().match(emailRegex)){
+        isEmailValid = true;
+        email = inputElement.value;
+        inputElement.value = "";
+        handleSuccessModal(isEmailValid, email);
+    } else {
+        isEmailValid = false;
+        inputElement.value = "";
+    }
+
+    return elementEmailValidation(isEmailValid);
+
+}
+
+const sendEmail = async () => {
+    emailjs.send()
+}
+
+const elementEmailValidation = (isEmailValid) => {
+
+    if(isEmailValid) {
+        inputElement.classList.remove("wrong-email");
+    } else {
+        inputElement.classList.add("wrong-email");
+    }
+
+}
+
+const handleSuccessModal = (isEmailValid, email) => {
+    if (isEmailValid) {
+        formElement.style.display = "none";
+        modalElement.style.display = "flex";
+        emailTextElement.innerHTML = `
+        A confirmation email has been sent to <strong>${email}</strong>. Please open it and click the button inside to confirm your subscription
+        `;
+    } else {
+        formElement.style.display = "flex";
+        modalElement.style.display = "none";
+    }
+}
+
+formElement.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    emailValidation();
+})
+
+modalElement.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    handleSuccessModal(false, "");
+})
